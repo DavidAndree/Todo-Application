e0f16251c5535a8552a1e5bc8edfb42cd56778b7
David Alvarado <davidalvaradoc0106@gmail.com>
2024-09-30 16:58:27 -0400

    Change model name to complete_by & make migrations

--- todos/models.py
+++ todos/models.py
@@ -8,8 +8,8 @@ from django.db import models
 
 
 class Todo(models.Model):
-    name = models.CharField(max_length=200)
-    due_date = models.DateField()
+    name = models.CharField(max_length=150)  # Adjusted based on the ERD
+    completed_by = models.DateTimeField()  # Changed to correct name
 
     def __str__(self):
         return self.name
