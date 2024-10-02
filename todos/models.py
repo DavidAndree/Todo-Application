2bf70a8c67ab135404b16a6f8d1b305e5b546abf
David Alvarado <davidalvaradoc0106@gmail.com>
2024-10-01 20:56:12 -0400

    Change model name to complete_by, make migrations, created super user again

--- todos/models.py
+++ todos/models.py
@@ -9,7 +9,7 @@ from django.db import models
 
 class Todo(models.Model):
     name = models.CharField(max_length=150)  # Adjusted based on the ERD
-    completed_by = models.DateTimeField()  # Changed to correct name
+    complete_by = models.DateTimeField()  # Changed to correct name
 
     def __str__(self):
         return self.name
